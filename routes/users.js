72d6f5790cf6b6d27c1b75f52d0f514077be3d3d
Olivier Bouvet <bouvet.olivier@gmail.com>
2021-04-15 14:48:34 +0200

    signup signin ok

--- routes/users.js
+++ routes/users.js
@@ -5,42 +5,52 @@ var UserModel = require('../models/users')
 
 /* SIGN IN // LOGIN */
 
-router.get('/sign-in', function(req, res, next) {
+router.post('/sign-in', async function(req, res, next) {
 
+  var searchUser = await UserModel.findOne({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  if(searchUser!= null){
+    req.session.user = {
+      name: searchUser.name,
+      firstname: searchUser.firstname,
+      email: searchUser.email,
+      password: searchUser.password
+    }
+    res.redirect('/recherche')
+  } else {
+    res.redirect("/")
+  }
+})
 
   
-  res.render('recherche');
-});
-
-
 /* SIGN UP // INSCRIPTION */
 
 router.post('/sign-up', async function(req, res, next) {
 
-    var newUser = new UserModel({
-      name: req.body.name,
-      firstname: req.body.firstname,
-      email: req.body.email,
-      password: req.body.password,
-    })
+  var searchUser = await UserModel.findOne({
+    email: req.body.email
+  })
+
+  if(!searchUser) {
+     var newUser = new UserModel({
+        name: req.body.name,
+        firstname: req.body.firstname,
+        email: req.body.email,
+        password: req.body.password,
+  })
   
     var newUserSave = await newUser.save();
-
     req.session.user = newUserSave
-    console.log(req.session.user)
-  
-    res.render('recherche')
-  
+    res.redirect('/recherche') 
+  } else {
+      res.redirect('/')
+  }
 })
 
-/* SIGN UP // INSCRIPTION */
 
-router.get('/logout', function(req,res,next){
-
-  req.session.user = null;
-
-  res.redirect('/')
-})
 
 
 module.exports = router;
